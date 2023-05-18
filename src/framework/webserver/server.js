69c46bff49446d18e0839ef10bf68cad4a6a64af
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-18 22:58:20 +0530

    added express and webserver starter pack and express configuration

---
+++ src/framework/webserver/server.js
@@ -0,0 +1,11 @@
+const serverConfig = (app, config) => {
+    const startServer = () => {
+        app.listen(config.port, () => {
+            console.log(`Server listening on Port ${config.port}`);
+        });
+    }; 
+    return { startServer };
+};
+
+export default serverConfig;
+    
